118913d7fe89640764fa8fbabeeb01d8ce60af91
vinu tv <vinutv91@gmail.com>
2022-05-31 11:30:05 +0530

    Add FAQ doc link

--- pages/index.js
+++ pages/index.js
@@ -889,7 +889,29 @@ export default function Home() {
                       </p>
                     </div>
                   </div>
-                  <div className="pt-10 lg:pt-16">
+                  <div className="pt-16">
+                    <a
+                      href="https://docs.google.com/document/d/e/2PACX-1vTj6_Kysry0_t6sXTt7ibzqBzxhLORr-iX2LBVaujbrlmnvkF8ZCPKX4HG0cWzdvl8r4F0E8smajKPl/pub"
+                      target="_blank"
+                      className="inline-flex justify-between items-center space-x-4 w-full p-4 border border-secondary-300 bg-gradient-to-r from-secondary-700 to-secondary-500 text-white hover:from-secondary-600 hover:to-secondary-400 rounded-lg text-lg font-semibold shadow-lg hover:underline hover:shadow-xl transition "
+                    >
+                      <span>Frequently Asked Questions</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                  <div className="pt-10 lg:pt-12">
                     <h4 className="text-secondary-300 text-xl lg:text-3xl  font-bold">
                       Questions?
                     </h4>
